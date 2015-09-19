1c8eed8e7f44756f98871ad8ab8597198e1ccb05
Justin Toniazzo <jutonz42@gmail.com>
2015-09-19 09:57:32 -0400

    Adds index fetcher

--- lib/projectionist/projects.rb
+++ lib/projectionist/projects.rb
@@ -1,7 +1,24 @@
-require "projectionist/projects/version"
+require "net/http"
+require "csv"
+require "byebug"
+
+require_relative "projects/version"
 
 module Projectionist
   module Projects
-    # Your code goes here...
+    INDEX_ROOT = "jeremywrowe.github.io"
+
+    def fetch_index
+      resp = Net::HTTP.get INDEX_ROOT, "/projectionist-projects/index.csv"
+      parse_index(resp)
+    end
+
+    def parse_index(index)
+      csv = CSV.new(index, headers: true, header_converters: :symbol)
+      csv.to_a.map do |row|
+        { row[0].to_sym => row[1] } # row = ["ember", "path/to/projection"]
+      end.reduce(&:merge)
+    end
+
   end
 end
