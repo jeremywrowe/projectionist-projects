7405bd8727e4bbb94990a476b60e7b39bb7ca171
Jeremy W. Rowe <jeremy.w.rowe@gmail.com>
2015-09-20 21:23:27 -0400

    wip

--- lib/projectionist/projects.rb
+++ lib/projectionist/projects.rb
@@ -5,12 +5,32 @@ require_relative "projects/version"
 
 module Projectionist
   module Projects
-    INDEX_ROOT = "jeremywrowe.github.io"
+    INDEX_ROOT         = "jeremywrowe.github.io"
+    DOWNLOAD_DIRECTORY = File.join(ENV["HOME"], ".projection-projects").to_s
 
     def self.fetch_projections
-      response = Net::HTTP.get INDEX_ROOT, "/projectionist-projects/index.csv"
-      CSV.new(response, headers: true, header_converters: :symbol)
+      csv_contents = Net::HTTP.get INDEX_ROOT, "/projectionist-projects-files/index.csv"
+      CSV.new(csv_contents, headers: true, header_converters: :symbol)
         .map(&:to_hash)
+    rescue Net::HTTP::SocketError
+      throw :server_unavailable
+    end
+
+    def self.download_projection(project:, ask_to_overwrite: true, stdin: -> { gets })
+      normalized_project = "#{project}.projections.json"
+      destination_file   = File.join(DOWNLOAD_DIRECTORY, normalized_project)
+
+      if ask_to_overwrite && File.exist? destination_file
+        print "Overwrite existing file? (y/N) "
+        return unless stdin.call.strip.downcase == "y"
+      end
+      
+      json_contents = Net::HTTP.get INDEX_ROOT, "/projectionist-projects-files/downloads/#{normalized_project}"
+      File.open('w', destination_file) do |file|
+        file.write json_contents
+      end
+    rescue Net::HTTP::SocketError
+      throw :server_unavailable
     end
   end
 end
--- spec/projectionist/projects_spec.rb
+++ spec/projectionist/projects_spec.rb
@@ -4,8 +4,8 @@ describe Projectionist::Projects do
   let(:resp) {
     [ 
       %w( project path                               updated ),
-      %w( ember   projections/ember.projections.json 2015-09-20),
-      %w( rails   projections/rails.projections.json 2015-11-20),
+      %w( ember   downloads/ember.projections.json 2015-09-20),
+      %w( rails   downloads/rails.projections.json 2015-11-20),
     ]
       .map { |row| row.join(",") }
       .join("\n")
@@ -13,14 +13,14 @@ describe Projectionist::Projects do
 
   before do
     allow(Net::HTTP).to receive(:get)
-      .with("jeremywrowe.github.io", "/projectionist-projects/index.csv")
+      .with("jeremywrowe.github.io", "/projectionist-projects-files/index.csv")
       .and_return(resp)
   end
 
   let(:results) { 
     [
-      { project: "ember", path: "projections/ember.projections.json", updated: "2015-09-20" },
-      { project: "rails", path: "projections/rails.projections.json", updated: "2015-11-20" }
+      { project: "ember", path: "downloads/ember.projections.json", updated: "2015-09-20" },
+      { project: "rails", path: "downloads/rails.projections.json", updated: "2015-11-20" }
     ]
   }
 
